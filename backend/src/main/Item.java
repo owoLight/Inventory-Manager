67fd18314e143054a34dc9a9c96815f2d47ca595
Samudra Gargo Bhattacharya <samudra6289@gmail.com>
2025-12-01 17:46:20 -0600

    Reset repository with new initial commit

---
+++ backend/src/main/Item.java
@@ -0,0 +1,29 @@
+public class Item {
+    protected String id;
+    protected String name;
+    protected String description;
+    public boolean isAvailable = true;
+
+    // constructor
+    public Item(String id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    // getters
+    public String getId(Item item) {
+        return item.id;
+    }
+    public String getName(Item item) {
+        return item.name;
+    }
+    public boolean checkAvailability(Item item) {
+        return item.isAvailable;
+    }
+
+    // setter
+    public void setAvailability(boolean bool) {
+        this.isAvailable = bool;
+    }
+}
