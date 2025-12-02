3da523a6e87651f8c4851da2b425ee26220c3f1b
owoLight <josiahapso@gmail.com>
2025-12-02 14:33:26 -0600

    hello

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello this is me trying to update code locally");
+        
     }
 }
 
